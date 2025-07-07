3138b5998a7faf8b5a2ca459ab99a64af01b6763
강민제 <985214344+minchey@users.noreply.github.com>
2025-07-08 01:04:48 +0900

    클라이언트 접속시 리스트에 추가

--- src/main/java/com/chatproject/secure_chat/server/ChatServer.java
+++ src/main/java/com/chatproject/secure_chat/server/ChatServer.java
@@ -2,7 +2,7 @@ package com.chatproject.secure_chat.server;
 
 import java.net.Socket;
 import java.net.ServerSocket;
-
+import java.util.ArrayList;
 import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -13,12 +13,20 @@ public class ChatServer {
     public static void main(String[] args) {
 
         try { //예외처리
+            ArrayList<String> clientList = new ArrayList<>();
             ServerSocket serverSocket = new ServerSocket(9999); //포트번호 9999로 서버 오픈
             System.out.println("서버실행 연결대기중...");
+            int client = 1;
 
             while (true) {
                 Socket clientSocket = serverSocket.accept(); //클라이언트 연결
                 System.out.println("클라이언트 연결됨");
+
+                String clientName = "client" + client; //클라이언트 접속시 리스트에 저장
+                clientList.add(clientName);
+                System.out.println(clientName + "님 입장");
+                client ++;
+
                 Thread thread = new Thread(new ClientMessageReader(clientSocket));
                 thread.start();
             }
