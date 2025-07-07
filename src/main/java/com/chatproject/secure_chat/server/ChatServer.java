56c21572550eca3a34351aef1234289613abacaa
강민제 <985214344+minchey@users.noreply.github.com>
2025-07-08 01:19:04 +0900

    array코드 수정

--- src/main/java/com/chatproject/secure_chat/server/ChatServer.java
+++ src/main/java/com/chatproject/secure_chat/server/ChatServer.java
@@ -16,14 +16,14 @@ public class ChatServer {
             ArrayList<String> clientList = new ArrayList<>();
             ServerSocket serverSocket = new ServerSocket(9999); //포트번호 9999로 서버 오픈
             System.out.println("서버실행 연결대기중...");
-            int client = 1;
+            int client = 1; //client 숫자
 
             while (true) {
                 Socket clientSocket = serverSocket.accept(); //클라이언트 연결
                 System.out.println("클라이언트 연결됨");
 
                 String clientName = "client" + client; //클라이언트 접속시 리스트에 저장
-                clientList.add(clientName);
+                clientList.add(clientName); //client 추가
                 System.out.println(clientName + "님 입장");
                 client ++;
 
