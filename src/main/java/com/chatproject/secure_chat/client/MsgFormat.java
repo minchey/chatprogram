282e3fbb356704e6bcedc781b614f27a65a3787c
강민제 <nolam@naver.com>
2025-07-10 23:45:28 +0900

    Json클래스 생성자 작성

---
+++ src/main/java/com/chatproject/secure_chat/client/MsgFormat.java
@@ -0,0 +1,38 @@
+package com.chatproject.secure_chat.client;
+
+import java.time.LocalDateTime;
+
+public class MsgFormat {
+    private String nickname;
+    private String msg;
+    private String timestamp;
+    public MsgFormat(){}
+    public MsgFormat(String nickname,String msg){
+
+        this.nickname = nickname;
+        this.msg = msg;
+        this.timestamp = LocalDateTime.now().toString();
+
+    }
+
+    public String getNickname(){
+        return nickname;
+    }
+    public void setNickname(String nickname){
+        this.nickname = nickname;
+    }
+
+    public String Msg(){
+        return msg;
+    }
+    public void setMsg(String msg){
+        this.msg = msg;
+    }
+
+    public String getTimestamp(){
+        return timestamp;
+    }
+    public void setTimestamp(String timestamp){
+        this.timestamp = timestamp;
+    }
+}
