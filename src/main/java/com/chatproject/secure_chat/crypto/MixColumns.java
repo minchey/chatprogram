bfb27ea87e5958aba71f5e8dc00051d33a2a63ff
강민제 <nolam@naver.com>
2025-07-14 19:50:39 +0900

    MixColumns 구현

--- src/main/java/com/chatproject/secure_chat/crypto/MixColumns.java
+++ src/main/java/com/chatproject/secure_chat/crypto/MixColumns.java
@@ -23,4 +23,27 @@ public class MixColumns {
     public static byte mulBy03(byte b){
         return (byte)(mulBy02(b) ^ (b & 0xFF));
     }
+
+    public static byte mul(byte a, byte b){
+        switch (a){
+            case 0x01: return mulBy01(b);
+            case 0x02: return mulBy02(b);
+            case 0x03: return mulBy03(b);
+            default: return 0;
+        }
+    }
+    public static byte[][] applyMixColumns(byte[][] state){
+        byte[][] result = new byte[4][4];
+        for(int col = 0; col<4; col++){
+            for(int row =0; row<4; row++){
+                result[row][col] = (byte)(
+                        mul(mixMatrix[row][0], state[0][col]) ^
+                        mul(mixMatrix[row][1], state[1][col]) ^
+                        mul(mixMatrix[row][2], state[2][col]) ^
+                        mul(mixMatrix[row][3], state[3][col])
+                        );
+            }
+        }
+        return result;
+    }
 }
