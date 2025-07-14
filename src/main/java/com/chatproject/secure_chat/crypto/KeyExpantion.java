3aa533272c677fb2cff0a55331c51886ce6758ac
강민제 <nolam@naver.com>
2025-07-14 22:11:00 +0900

    KeyExpantion 첫번쨰 라운드 word 구현

--- src/main/java/com/chatproject/secure_chat/crypto/KeyExpantion.java
+++ src/main/java/com/chatproject/secure_chat/crypto/KeyExpantion.java
@@ -1,4 +1,15 @@
 package com.chatproject.secure_chat.crypto;
 
 public class KeyExpantion {
+    public static byte[][][] generateRoundKeys(byte [] key){
+        byte[][] word = new byte[44][4];
+
+        for(int i = 0; i<4; i++){
+            word[i][0] = key[i*4];
+            word[i][1] = key[i*4+1];
+            word[i][2] = key[i*4+2];
+            word[i][3] = key[i*4+3];
+        };
+
+    }
 }
