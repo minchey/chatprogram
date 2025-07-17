3779138a95c4f3d98c2de351072873b9faa42319
강민제 <nolam@naver.com>
2025-07-17 23:01:59 +0900

    RSA 전체 구현

--- src/main/java/com/chatproject/secure_chat/crypto/RSAUtil.java
+++ src/main/java/com/chatproject/secure_chat/crypto/RSAUtil.java
@@ -10,4 +10,16 @@ public class RSAUtil {
         generator.initialize(2048);
         return generator.generateKeyPair();
     }
+    public static String encrypt(String plainText, PublicKey publicKey) throws Exception{
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] encrypted = cipher.doFinal(plainText.getBytes());
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+    public static String decrypt(String encryptedText, PrivateKey privateKey) throws Exception{
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] decrypted = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
+        return new String(decrypted);
+    }
 }
