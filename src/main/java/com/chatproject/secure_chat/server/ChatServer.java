9ebfb60fabffade8693bef966b5326ff0b3c4167
강민제 <nolam@naver.com>
2025-07-18 00:39:18 +0900

    공개키 HashMap에 저장2

--- src/main/java/com/chatproject/secure_chat/server/ChatServer.java
+++ src/main/java/com/chatproject/secure_chat/server/ChatServer.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.ServerSocket;
+import java.security.KeyFactory;
 import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.*;
 
 import com.chatproject.secure_chat.client.ChatClient;
@@ -13,7 +15,6 @@ import com.chatproject.secure_chat.server.ClientInfo;
 public class ChatServer {
     public static List<ClientInfo> clientList = Collections.synchronizedList(new ArrayList<>());
     public static Map<String, PublicKey> publicKeyMap = new HashMap<>();
-    private static PublicKey pubKey;
     public static void main(String[] args) {
 
 
@@ -24,16 +25,27 @@ public class ChatServer {
             while (true) {
                 Socket clientSocket = serverSocket.accept();//클라이언트 연결
                 BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                String nickname = br.readLine();
-                ClientInfo clientInfo = new ClientInfo(nickname, clientSocket, pubKey);
+                String nickname = br.readLine(); //닉네임 받기
+                String base64Key = br.readLine(); //공개키 받기
+
+                //Base64 -> Bytes -> PublicKey 복원
+                byte[] keyBytes = Base64.getDecoder().decode(base64Key);
+                X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+                KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+                PublicKey publicKey = keyFactory.generatePublic(spec);
+
+                //공개키 HashMap에 저장
+                publicKeyMap.put(nickname, publicKey);
+
+                ClientInfo clientInfo = new ClientInfo(nickname, clientSocket, publicKey);
                 clientList.add(clientInfo);
                 System.out.println(nickname + "님 연결됨");
-                Thread thread = new Thread(new ClientMessageReader(clientSocket, nickname, pubKey));
+                Thread thread = new Thread(new ClientMessageReader(clientSocket, nickname, publicKey));
                 thread.start();
             }
 
             //serverSocket.close(); //서버 종료
-        } catch (Exception e) { //오류처리
+        } catch (Exception e) { //예외처리
             e.printStackTrace();
         }
 
